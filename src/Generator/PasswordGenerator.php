c119113e8ba68bc19e529b573921b8ebae042f01
Burak Bolat <burak.bolat@hprotravel.com>
2022-12-20 01:03:01 +0300

    Rename method param

--- src/Generator/PasswordGenerator.php
+++ src/Generator/PasswordGenerator.php
@@ -61,13 +61,13 @@ class PasswordGenerator implements GeneratorInterface
         return implode('', $acronymList);
     }
 
-    private function convertToNumeralAndSpecialChar(mixed $firstChar): string|array
+    private function convertToNumeralAndSpecialChar(mixed $char): string|array
     {
         // A a B b C c D d E e F f G g H h I i J j K k L l M m N n O o P p Q q R r S s T t U u V v W w X x Y y Z z
         return str_ireplace(
             ['a', 'e', 'i', 'l', 'o', 's'],
             ['@', '3', '!', '1', '0', '5'],
-            $firstChar
+            $char
         );
     }
 }
