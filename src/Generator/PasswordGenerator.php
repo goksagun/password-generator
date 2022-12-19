e49552735e50536edb198fbd5728dbe7c2159d9b
Burak Bolat <burak.bolat@hprotravel.com>
2022-12-20 01:04:19 +0300

    Re-order methods

--- src/Generator/PasswordGenerator.php
+++ src/Generator/PasswordGenerator.php
@@ -56,11 +56,6 @@ class PasswordGenerator implements GeneratorInterface
         return $listAcronym;
     }
 
-    private function concat(array $acronymList): string
-    {
-        return implode('', $acronymList);
-    }
-
     private function convertToNumeralAndSpecialChar(mixed $char): string|array
     {
         // A a B b C c D d E e F f G g H h I i J j K k L l M m N n O o P p Q q R r S s T t U u V v W w X x Y y Z z
@@ -69,5 +64,10 @@ class PasswordGenerator implements GeneratorInterface
             ['@', '3', '!', '1', '0', '5'],
             $char
         );
+    }
+
+    private function concat(array $acronymList): string
+    {
+        return implode('', $acronymList);
     }
 }
