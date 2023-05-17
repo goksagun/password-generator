549a18bd9e73dcb187af8f385d32014c3f7aeb96
Burak Bolat <burak.bolat@hprotravel.com>
2023-05-17 18:36:58 +0300

    Rename DefaultController as HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_default')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('default/index.html.twig');
+        return $this->render('home/index.html.twig');
     }
 }
